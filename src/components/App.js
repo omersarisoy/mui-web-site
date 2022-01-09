c857d5607d0c21d550e48ecaaa00604748f966ec
Ömer Sarısoy <omer.sarisoy@asnak.com>
2022-01-10 00:10:24 +0300

    lottie

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@material-ui/styles";
 import theme from "./ui/Theme";
 import Header from "./ui/Header";
 import Footer from "./ui/Footer";
+import LandingPage from "./LandingPage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
         <Switch>
-          <Route exact path="/" component={() => <div style={{height:2000}}>Home</div>} />
+          <Route exact path="/" component={LandingPage} />
           <Route exact path="/services" component={() => <div>Services</div>} />
           <Route
             exact
